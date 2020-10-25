5b9877f8891c10a2be08f98656b1d58f88155613
Isaac-Graham <ccccym666@gmail.com>
2020-10-26 01:55:44 +0800

    refactor impsetup (it is too ugly before)

--- src/main/java/org/ai/carp/IMPSetup.java
+++ src/main/java/org/ai/carp/IMPSetup.java
@@ -25,53 +25,89 @@ import java.util.Scanner;
 public class IMPSetup {
 
     private static final Logger logger = LoggerFactory.getLogger(IMPSetup.class);
+    /**
+     * This csv file is a overall description for all datasets of imp
+     * It is in the form of {filename},{seedCount},{model},{time},{memory},{cpu}
+     * Space between each item is not allowed
+     */
+    private static final String impDatasetCsv = "datasets_imp.csv";
+    /**
+     * This csv file is a overall description for all datasets of imp
+     * It is in the form of {filename},{seedfile},{model},{limitTime},{memory},{cpu},{influence},{bias}
+     * Space between each item is not allowed
+     */
+    private static final String iseDatasetCsv = "datasets_ise.csv";
+    // All of the dataset files, which end with .txt, are listed under the folder below
+    private static final String impDataFolder = "datasets_imp";
+    private static final String iseDataFolder = "datasets_ise";
 
     public static void main(String[] args) throws URISyntaxException {
         SpringApplication app = new SpringApplication(IMPSetup.class);
         app.setWebApplicationType(WebApplicationType.NONE);
         app.run(args);
-        // addISEDatasets();
-        // addIMPDatasets();
-        // enableIseDatasets(true);
+//        addISEDatasets();
+        addIMPDatasets();
+//        enableIseDatasets(true);
+    }
+
+    private static void addIMPDatasets() throws URISyntaxException {
+        Map<String, File> fileMap = getDataFileMap(impDataFolder);
+        Scanner scanner = getDataScanner(impDatasetCsv);
+        if (scanner == null) {
+            return;
+        }
+//        name, seedCount, model, limitTime, memory, cpu,
+        while (scanner.hasNextLine()) {
+            String[] split = splitCsvData(scanner.nextLine());
+            if (split == null || split.length == 0) {
+                continue;
+            }
+            String name = split[0] + "-" + split[1] + "-" + split[2];
+            if (Database.getInstance().getImpDatasets().findDatasetByName(name) != null) {
+                continue;
+            }
+            try {
+                File networkFile = fileMap.get(split[0]);
+                String network = new Scanner(networkFile).useDelimiter("\\Z").next().replace("\r", "");
+                IMPDataset dataset = new IMPDataset(name, Integer.valueOf(split[3])
+                        , Integer.valueOf(split[4]), Integer.valueOf(split[5])
+                        , Integer.valueOf(split[1]), split[2], network);
+                dataset.setEnabled(true);
+                dataset.setSubmittable(true);
+                dataset.setFinalJudge(false);
+                dataset = Database.getInstance().getImpDatasets().insert(dataset);
+                logger.info(dataset.toString());
+            } catch (Exception e) {
+                logger.error("Error adding dataset", e);
+            }
+        }
     }
 
     private static void addISEDatasets() throws URISyntaxException {
-        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-        InputStream is = classLoader.getResourceAsStream("datasets_ise.csv");
-        if (is == null) {
-            logger.error("datasets_ise.csv not found");
+        Map<String, File> fileMap = getDataFileMap(iseDataFolder);
+        Scanner scanner = getDataScanner(iseDatasetCsv);
+        if (scanner == null) {
             return;
-        }
-        Scanner scanner = new Scanner(is);
-        File datasets = new File(classLoader.getResource("datasets_imp").toURI());
-        File[] list = datasets.listFiles((dir, name) -> name.endsWith(".txt"));
-        Map<String, File> fileMap = new HashMap<>();
-        for (File f : list) {
-            fileMap.put(f.getName().replace(".txt", ""), f);
         }
 //        name, seedfile, model, limitTime, memory, cpu, influence, bias
         while (scanner.hasNextLine()) {
-            String line = scanner.nextLine();
-            if(line.startsWith("#"))
-                continue;
-            line = line.replaceAll("\r", "");
-            String[] splitted = line.split(",");
-            if (StringUtils.isEmpty(splitted[0])) {
+            String[] split = splitCsvData(scanner.nextLine());
+            if (split == null || split.length == 0) {
                 continue;
             }
-            String name = splitted[0] + "-" + splitted[1] + "-" + splitted[2];
+            String name = split[0] + "-" + split[1] + "-" + split[2];
             if (Database.getInstance().getIseDatasets().findDatasetByName(name) != null) {
                 continue;
             }
             try {
-                File networkFile = fileMap.get(splitted[0]);
+                File networkFile = fileMap.get(split[0]);
                 String network = new Scanner(networkFile).useDelimiter("\\Z").next().replace("\r", "");
-                File seedsFile = fileMap.get(splitted[1]);
+                File seedsFile = fileMap.get(split[1]);
                 String seeds = new Scanner(seedsFile).useDelimiter("\\Z").next().replace("\r", "");
-                ISEDataset dataset = new ISEDataset(name, Integer.valueOf(splitted[3])
-                        , Integer.valueOf(splitted[4]), Integer.valueOf(splitted[5])
-                        , splitted[2], network, seeds, Double.valueOf(splitted[6])
-                        , Double.valueOf(splitted[7]));
+                ISEDataset dataset = new ISEDataset(name, Integer.valueOf(split[3])
+                        , Integer.valueOf(split[4]), Integer.valueOf(split[5])
+                        , split[2], network, seeds, Double.valueOf(split[6])
+                        , Double.valueOf(split[7]));
                 dataset.setEnabled(true);
                 dataset.setSubmittable(true);
                 dataset.setFinalJudge(false);
@@ -90,9 +126,9 @@ public class IMPSetup {
             c.setEnabled(true);
             c.setSubmittable(true);
             c.setFinalJudge(false);
-            if(c.getName().startsWith("NetHEPT")){
+            if (c.getName().startsWith("NetHEPT")) {
                 c.setMemory(2000);
-                logger.info("change memory"+c.toString());
+                logger.info("change memory" + c.toString());
             }
             Database.getInstance().getImpDatasets().save(c);
             logger.info(c.toString());
@@ -108,49 +144,36 @@ public class IMPSetup {
         });
     }
 
-    private static void addIMPDatasets() throws URISyntaxException {
+    private static Scanner getDataScanner(String csvFile) {
         ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-        InputStream is = classLoader.getResourceAsStream("datasets_imp.csv");
+        InputStream is = classLoader.getResourceAsStream(csvFile);
         if (is == null) {
-            logger.error("datasets_imp.csv not found");
-            return;
+            logger.error(String.format("%s not found", csvFile));
+            return null;
         }
-        Scanner scanner = new Scanner(is);
-        File datasets = new File(classLoader.getResource("datasets_imp").toURI());
+        return new Scanner(is);
+    }
+
+    private static Map<String, File> getDataFileMap(String folder) throws URISyntaxException {
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        File datasets = new File(classLoader.getResource(folder).toURI());
         File[] list = datasets.listFiles((dir, name) -> name.endsWith(".txt"));
         Map<String, File> fileMap = new HashMap<>();
         for (File f : list) {
             fileMap.put(f.getName().replace(".txt", ""), f);
         }
-//        name, seedCount, model, limitTime, memory, cpu,
-        while (scanner.hasNextLine()) {
-            String line = scanner.nextLine();
-            if(line.startsWith("#"))
-                continue;
-            line = line.replaceAll("\r", "");
-            String[] splitted = line.split(",");
-            if (StringUtils.isEmpty(splitted[0])) {
-                continue;
-            }
-            String name = splitted[0] + "-" + splitted[1] + "-" + splitted[2];
-            if (Database.getInstance().getImpDatasets().findDatasetByName(name) != null) {
-                continue;
-            }
-            try {
-                File networkFile = fileMap.get(splitted[0]);
-                String network = new Scanner(networkFile).useDelimiter("\\Z").next().replace("\r", "");
-                IMPDataset dataset = new IMPDataset(name, Integer.valueOf(splitted[3])
-                        , Integer.valueOf(splitted[4]), Integer.valueOf(splitted[5])
-                        , Integer.valueOf(splitted[1]), splitted[2], network);
-                dataset.setEnabled(true);
-                dataset.setSubmittable(true);
-                dataset.setFinalJudge(false);
-                dataset = Database.getInstance().getImpDatasets().insert(dataset);
-                logger.info(dataset.toString());
-            } catch (Exception e) {
-                logger.error("Error adding dataset", e);
-            }
-        }
+        return fileMap;
     }
 
+    private static String[] splitCsvData(String lineData) {
+        if (lineData.startsWith("#")) {
+            return null;
+        }
+        lineData = lineData.replaceAll("\r", "");
+        String[] split = lineData.split(",");
+        if (StringUtils.isEmpty(split[0])) {
+            return null;
+        }
+        return split;
+    }
 }
